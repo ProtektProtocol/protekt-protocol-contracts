cc594e28feb72e99849d35205a9bb7ba95e354cd
Corbin Page <corbin.page@gmail.com>
2020-10-28 11:46:01 -0400

    Updated IClaimsManager.sol

--- interfaces/protekt/IClaimsManager.sol
+++ interfaces/protekt/IClaimsManager.sol
@@ -1,10 +1,27 @@
 pragma solidity ^0.5.17;
 
 interface IClaimsManager {
+	enum ClaimsStatus {
+		Active,
+		Investigating,
+		ClaimAccepted
+	}
+
+	// Setters
+	function setShieldToken(address _shieldToken) external;
+	function setGovernance(address _governance) external;
 	function setInvestigationPeriodBlocks(uint256 _period) external;
 
+	// Claims Management
 	function checkPayoutEvent() external view returns (bool);
 	function submitClaim() external;
 	function payoutClaim() external;
+
+	// Variables
+	function governance() external returns (address);
+	function status() external returns (ClaimsStatus);
+	function investigationPeriod() external returns (uint256);
+	function currentInvestigationPeriodEnd() external returns (uint256);
+	function activePayoutEvent() external returns (bool);
 }
 
